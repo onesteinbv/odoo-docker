977b94df68f730e45652ab4d9e396cc80a4a3fda
tarteo <d.sluijk@onestein.nl>
2023-08-30 11:04:46 +0200

    Test enable database manager

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -37,11 +37,11 @@ else
 fi
 
 # TODO this could (should?) be sourced from file(s) under confd control
-export PGHOST=${DB_HOST}
-export PGPORT=${DB_PORT:-5432}
-export PGUSER=${DB_USER}
-export PGPASSWORD=${DB_PASSWORD}
-export PGDATABASE=${DB_NAME}
+#export PGHOST=${DB_HOST}
+#export PGPORT=${DB_PORT:-5432}
+#export PGUSER=${DB_USER}
+#export PGPASSWORD=${DB_PASSWORD}
+#export PGDATABASE=${DB_NAME}
 
 if [[ -z "$DB_NAME" || "$DB_NAME" == "False" ]]; then
   echo "No DB_NAME environment variable: Skipping update";
